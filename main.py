38c56840360dee2d1b520b6ae65c0947970b59e1
Daniel <pavlovdaniel86@gmail.com>
2022-08-16 22:25:29 +0300

    Add button adds data to text file.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from tkinter import *
+from tkinter import messagebox
 
-WHITE = "#EDEADE"
+# Create window
 
 window = Tk()
-
 window.title("Password Manager")
-
 window.config(padx=50, pady=100)
+
+
+# ---------------------------- FUNCTIONS ------------------------------- #
+
+
+def add_pass():
+    with open("passwords.txt", "a") as data:
+
+        # Get the info from entry fields
+        website_get = Entry.get(e_website)
+        email_username_get = Entry.get(e_website)
+        password_get = Entry.get(e_password)
+
+        # Write info into text file
+        data.write(f"{website_get}  |  {email_username_get}  |  {password_get} \n")
+
+        # Clear fields after adding them to text file
+        e_website.delete(0, END)
+        e_emailUsername.delete(0, END)
+        e_password.delete(0, END)
+
+
+
+# ---------------------------- MAKING THE GUI ------------------------------- #
 
 
 # Canvas
@@ -26,7 +49,9 @@ e_password = Entry(width=33)
 
 # Buttons
 genPass = Button(text="Generate Password")
-add = Button(text="Add", width=43)
+add = Button(text="Add", width=43, command=add_pass)
+
+# ---------------------------- GRID PLACEMENT ------------------------------- #
 
 # Grid Layout
 canvas.grid(row=0, column=1)
@@ -38,6 +63,5 @@ e_emailUsername.grid(row=2, column=1, columnspan=2)
 e_password.grid(row=3, column=1)
 genPass.grid(row=3, column=2)
 add.grid(row=4, column=1, columnspan=3)
-
 
 window.mainloop()
